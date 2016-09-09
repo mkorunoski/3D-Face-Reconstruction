a4c4030a10b3072b153fa4514f95d5f93e3cd98f
mkorunoski <mkorunoski@hotmail.com>
2016-09-09 19:43:21 +0200

    Finishing touches.

--- 3D Face Reconstruction/3D Face Reconstruction/Main.cpp
+++ 3D Face Reconstruction/3D Face Reconstruction/Main.cpp
@@ -29,80 +29,97 @@
 #include <GL\glew.h>
 #include <SDL2\SDL.h>
 #include <glm\glm.hpp>
+#include <glm\gtx\transform.hpp>
 #include <glm\gtc\matrix_transform.hpp>
 #include <glm\gtc\type_ptr.hpp>
 #include <glm\gtx\euler_angles.hpp>
+#include <glm\gtx\string_cast.hpp>
 
 #include "OBJLoader.h"
 
+// ==================================================
+// POSE ESTIMATION INCLUDES
+// ==================================================
+#include "RPP.h"
+
 const char* TITLE  = "3D Face Reconstruction";
-const int	WIDTH  = 800;
+const int	WIDTH  = 600;
 const int	HEIGHT = 600;
 
 const GLchar* VERTEX_SHADER_SOURCE[] = {
-	"#version 330 core										\n"
-	"														\n"
-	"layout(location = 0) in vec3 position;					\n"
-	"layout(location = 1) in vec3 color;					\n"
-	"														\n"
-	"uniform bool calcColor;								\n"
-	"uniform mat4 M;										\n"
-	"uniform mat4 VP;										\n"
-	"														\n"
-	"out vec4 f_color;										\n"
-	"														\n"
-	"void main()											\n"
-	"{														\n"
-	"   if(calcColor)										\n"
-	"	   f_color = vec4(clamp(position, 0, 1), 1);		\n"
-	"	else												\n"
-	"	   f_color = vec4(color, 1);						\n"
-	"	gl_Position = VP * M * vec4(position, 1);			\n"
-	"}														\n"
+	"#version 330 core																				\n"
+	"																								\n"
+	"layout(location = 0) in vec3 position;															\n"
+	"layout(location = 1) in vec3 color;															\n"
+	"																								\n"
+	"uniform bool calcColor;																		\n"
+	"uniform mat4 M;																				\n"
+	"uniform mat4 VP;																				\n"
+	"																								\n"	
+	"out vec2 f_texCoords;																			\n"
+	"																								\n"
+	"float map(float val, float A, float B, float a, float b)										\n"
+	"{																								\n"
+	"	return (val - A) * (b - a) / (B - A) + a;													\n"
+	"}																								\n"
+	"																								\n"
+	"void main()																					\n"
+	"{																								\n"
+	"	vec4 ndc = VP * M * vec4(position, 1);														\n"
+	"	gl_Position = ndc;																			\n"
+	"	float u = map(ndc.x / ndc.w, -1, 1, 0, 1);													\n"
+	"	float v = map(ndc.y / ndc.w, -1, 1, 0, 1);													\n"
+	"	f_texCoords = vec2(u, v);																	\n"
+	"}																								\n"
 };
 const GLchar* FRAGMENT_SHADER_SOURCE[] = {
-	"#version 330 core										\n"
-	"														\n"
-	"in vec4 f_color;										\n"
-	"														\n"
-	"out vec4 fragColor;									\n"
-	"														\n"
-	"void main()											\n"
-	"{														\n"
-	"	fragColor = f_color;								\n"
-	"}														\n"
+	"#version 330 core																				\n"
+	"																								\n"	
+	"in vec2 f_texCoords;																			\n"
+	"																								\n"
+	"uniform sampler2D sampler;																		\n"
+	"																								\n"
+	"out vec4 fragColor;																			\n"
+	"																								\n"
+	"void main()																					\n"
+	"{																								\n"	
+	"	fragColor = texture(sampler, f_texCoords);													\n"
+	"}																								\n"
 };
 
-/*
-Лево око надвор		37 - 1 (+4.65754, +3.71519, -0.01428)
-Лево око внатре		40 - 1 (+1.97604, +3.42160, +0.23674)
-Десно око надвор	46 - 1 (-4.65754, +3.71519, -0.01428)
-Десно око внатре	43 - 1 (-1.97604, +3.42160, +0.23674)
-Нос лево			32 - 1 (+1.84389, -0.29071, +1.91656)
-Нос десно			36 - 1 (-1.84389, -0.29071, +1.91656)
-Уста лево			49 - 1 (+2.61255, -3.36301, +1.04384)
-Уста десно			55 - 1 (-2.61255, -3.36301, +1.04384)
-Брада				 9 - 1 (+0.00000, -7.02612, +1.70428)
-*/
+//feature			index
+//Лево око надвор	36
+//Лево око внатре	39
+//Десно око надвор	45
+//Десно око внатре	42
+//Нос лево			31
+//Нос десно			35
+//Уста лево			48
+//Уста десно		54
+//Брада				8
 
 const cv::Point3f modelPointsArr[] =
 {
-	cv::Point3f(+4.65754f, +3.71519f, -0.01428f),
-	cv::Point3f(+1.97604f, +3.42160f, +0.23674f),
-	cv::Point3f(-4.65754f, +3.71519f, -0.01428f),
-	cv::Point3f(-1.97604f, +3.42160f, +0.23674f),
-	cv::Point3f(+1.84389f, -0.29071f, +1.91656f),
-	cv::Point3f(-1.84389f, -0.29071f, +1.91656f),
-	cv::Point3f(+2.61255f, -3.36301f, +1.04384f),
-	cv::Point3f(-2.61255f, -3.36301f, +1.04384f),
-	cv::Point3f(+0.00000f, -7.02612f, +1.70428f)
+	cv::Point3f(-1.13642, -0.90731, +0.00391),
+	cv::Point3f(-0.48241, -0.83527, -0.05782),
+	cv::Point3f(+1.13642, -0.90731, +0.00391),
+	cv::Point3f(+0.48241, -0.83527, -0.05782),
+	cv::Point3f(-0.44994, +0.07104, -0.46759),
+	cv::Point3f(+0.44994, +0.07104, -0.46759),
+	cv::Point3f(-0.63738, +0.81876, -0.25444),
+	cv::Point3f(+0.63738, +0.81876, -0.25444),
+	cv::Point3f(-0.00005, +1.85261, -0.27220)
 };
+
+glm::mat4 getRotation(cv::Mat& img, const std::vector<cv::Point2f>& srcImagePoints,
+	const std::vector<cv::Point3f>& modelPoints);
 
 void getEulerAngles(
 	const std::vector<cv::Point2f>& srcImagePoints,
 	const std::vector<cv::Point3f>& modelPoints,
 	const cv::Mat& srcImage,
-	cv::Vec3d& eav
+	glm::mat3& rotationMatrix,
+	glm::vec3& translationVector
 	);
 
 // ==================================================
@@ -124,7 +141,8 @@ int main(int argc, char** argv)
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glClearColor(0.8, 0.8, 0.8, 1);
 	glEnable(GL_DEPTH_TEST);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glFrontFace(GL_CCW);
+	glCullFace(GL_BACK);
 
 	GLuint vertexShader;
 	GLuint fragmentShader;
@@ -180,37 +198,12 @@ int main(int argc, char** argv)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
 	glBindVertexArray(0);
 	
-	GLfloat axes[] =
-	{
-		0, 0, 0,	1, 0, 0,
-		1, 0, 0,	1, 0, 0,
-
-		0, 0, 0,	0, 1, 0,
-		0, 1, 0,	0, 1, 0,
-
-		0, 0, 0,	0, 0, 1,
-		0, 0, 1,	0, 0, 1
-	};
-	GLuint axesVAO;
-	GLuint axesVBO;
-	glGenVertexArrays(1, &axesVAO);
-	glGenBuffers(1, &axesVBO);
-	glBindVertexArray(axesVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, axesVBO);
-	glBufferData(GL_ARRAY_BUFFER, 6 * 6 * sizeof(GL_FLOAT), &axes[0], GL_STATIC_DRAW);	
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
-	glBindVertexArray(0);
-
 	glm::mat4 model			 = glm::mat4(1.0);
-	glm::mat4 view			 = glm::lookAt(glm::vec3(5, 1, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 view			 = view = glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(0, 0, 1), glm::vec3(0, -1, 0));
 	glm::mat4 projection	 = glm::perspective(70.0f, (float)WIDTH / (float)HEIGHT, 0.1f, 1000.0f);
 	glm::mat4 viewProjection = projection * view;
 	GLuint uMLocation  = glGetUniformLocation(program, "M");
-	GLuint uVPLocation = glGetUniformLocation(program, "VP");
-	GLuint uCalcColor  = glGetUniformLocation(program, "calcColor");
+	GLuint uVPLocation = glGetUniformLocation(program, "VP");	
 		
 	std::vector<cv::Point3f> modelPoints(modelPointsArr, modelPointsArr + sizeof(modelPointsArr) / sizeof(modelPointsArr[0]));
 
@@ -221,84 +214,108 @@ int main(int argc, char** argv)
 	cv::Mat srcImageCV;
 	dlib::image_window win;
 
-	/*
-	cv::VideoCapture vc(0);
-	if (!vc.isOpened())
-	{
-		std::cerr << "ERROR: VideoCapture failed to initialize.\n" << std::endl;
-		return -1;
-	}
-	*/
-
 	dlib::array2d<dlib::rgb_pixel> srcImageDLIB;
 	std::vector<dlib::rectangle> dets;
 	std::vector<dlib::full_object_detection> shapes;
 	dlib::full_object_detection shape;
+
+	srcImageCV = cv::imread("face1.jpg");
+	dlib::assign_image(srcImageDLIB, dlib::cv_image<dlib::bgr_pixel>(srcImageCV));
+	dets = detector(srcImageDLIB);
+	shape = sp(srcImageDLIB, dets[0]);
+	std::vector<cv::Point2f> srcImagePoints;
+	int i;
+	i = 36; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 39; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 45; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 42; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 31; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 35; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 48; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 54; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	i = 8;	srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
+	shapes.push_back(shape);
+	
+	win.clear_overlay();
+	win.set_image(srcImageDLIB);
+	win.add_overlay(dlib::render_face_detections(shapes));
+		
+	glm::mat3 rotationMatrix;
+	glm::vec3 translationVector;
+	getEulerAngles(srcImagePoints, modelPoints, srcImageCV, rotationMatrix, translationVector);
+	glm::mat4 translationMatrix = glm::translate(translationVector);
+	model = translationMatrix * 
+		glm::mat4(
+			rotationMatrix[0][0], rotationMatrix[0][1], rotationMatrix[0][2], 0,
+			rotationMatrix[1][0], rotationMatrix[1][1], rotationMatrix[1][2], 0,
+			rotationMatrix[2][0], rotationMatrix[2][1], rotationMatrix[2][2], 0,
+			0,					  0,					0,					  1
+		);	
+	std::cout << glm::to_string(translationVector) << std::endl;
+	
+	GLuint srcImageTexture;
+	if (srcImageCV.empty())
+	{
+		std::cout << "image empty" << std::endl;
+	}
+	else
+	{
+		cv::flip(srcImageCV, srcImageCV, 0);
+		glGenTextures(1, &srcImageTexture);
+		glBindTexture(GL_TEXTURE_2D, srcImageTexture);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, srcImageCV.cols, srcImageCV.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, srcImageCV.ptr());
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	
+	float angle = 0.0f;
+	SDL_Event e;
 	boolean run = true;
 	while (run)
-	{
-		SDL_Event e;
+	{		
 		SDL_PollEvent(&e);
 		if (e.type == SDL_QUIT)
 			run = false;
-
+		if (e.type == SDL_KEYDOWN)
+		{
+			switch (e.key.keysym.sym)
+			{
+			case SDLK_LEFT:
+				angle -= 0.1f;
+				break;
+			case SDLK_RIGHT:
+				angle += 0.1f;
+				break;
+			default:
+				break;
+			}
+		}		
+		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(program);
 		glProgramUniformMatrix4fv(program, uVPLocation, 1, false, glm::value_ptr(viewProjection));
-
-		/*
-		vc.read(srcImageCV);	
-		*/
-		srcImageCV = cv::imread("face.jpg");		
-		dlib::assign_image(srcImageDLIB, dlib::cv_image<dlib::bgr_pixel>(srcImageCV));
-		dets = detector(srcImageDLIB);		
-		shape = sp(srcImageDLIB, dets[0]);				
-		std::vector<cv::Point2f> srcImagePoints;
-		int i;
-		i = 36; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 39; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 45; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 42; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 31; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 35; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 48; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 54; srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		i = 8;	srcImagePoints.push_back(cv::Point2f(shape.part(i).x(), shape.part(i).y()));
-		shapes.push_back(shape);
-		/*
-		for (int i = 0; i < srcImagePoints.size(); ++i)
-		{
-			cv::circle(srcImageCV, srcImagePoints.at(i), 5, cv::Scalar(0, 0, 1));
-		}
-		cv::imshow("Faces", srcImageCV);
-		*/
-		win.clear_overlay();
-		win.set_image(srcImageDLIB);
-		win.add_overlay(dlib::render_face_detections(shapes));
-		cv::Vec3d eav;
-		getEulerAngles(srcImagePoints, modelPoints, srcImageCV, eav);
-		/*
-		yaw   y
-		pitch x
-		roll  z
-		*/
-		model = glm::eulerAngleYXZ(eav[1], eav[0], eav[2]);		
-		glProgramUniformMatrix4fv(program, uMLocation, 1, false, glm::value_ptr(model));		
-		glBindVertexArray(faceVAO);
-		glUniform1i(uCalcColor, true);		
-		glLineWidth(1);
+		glProgramUniformMatrix4fv(program, uMLocation, 1, false, glm::value_ptr(model));
+				
+		glBindTexture(GL_TEXTURE_2D, 0);		
+		glBindVertexArray(faceVAO);		
+		glLineWidth(2);		
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
-		model = glm::mat4(1);
-		glProgramUniformMatrix4fv(program, uMLocation, 1, false, glm::value_ptr(model));		
-		glBindVertexArray(axesVAO);
-		glUniform1i(uCalcColor, false);
-		glLineWidth(3);
-		glDrawArrays(GL_LINES, 0, 12);
+		glBindTexture(GL_TEXTURE_2D, srcImageTexture);
+		glBindVertexArray(faceVAO);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
-
+				
 		SDL_GL_SwapWindow(window);
 	}
 		
@@ -313,16 +330,17 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+// http://www.morethantechnical.com/2012/10/17/head-pose-estimation-with-opencv-opengl-revisited-w-code/
 void getEulerAngles(
 	const std::vector<cv::Point2f>& srcImagePoints,
 	const std::vector<cv::Point3f>& modelPoints,
 	const cv::Mat& srcImage,
-	cv::Vec3d& eav
+	glm::mat3& rotationMatrix,
+	glm::vec3& translationVector
 	)
 {
 	std::vector<double> rv(3), tv(3);
-	cv::Mat rvec(rv), tvec(tv);
-	
+	cv::Mat rvec(rv), tvec(tv);	
 
 	cv::Mat ip(srcImagePoints);
 	cv::Mat op = cv::Mat(modelPoints);
@@ -355,12 +373,19 @@ void getEulerAngles(
 	cv::Mat rotM(3, 3, CV_64FC1, rot);
 	Rodrigues(rvec, rotM);
 	double* _r = rotM.ptr<double>();
-	/*
-	printf("rotation mat: \n %.3f %.3f %.3f\n%.3f %.3f %.3f\n%.3f %.3f %.3f\n",
-	 	_r[0], _r[1], _r[2], _r[3], _r[4], _r[5], _r[6], _r[7], _r[8]);
+	
+	// printf("rotation mat: \n %.3f %.3f %.3f\n%.3f %.3f %.3f\n%.3f %.3f %.3f\n", _r[0], _r[1], _r[2], _r[3], _r[4], _r[5], _r[6], _r[7], _r[8]);
+	
+	rotM = rotM.t();
+	
+	rotationMatrix = glm::mat3(
+		(float)rot[0], (float)rot[1], (float)rot[2],
+		(float)rot[3], (float)rot[4], (float)rot[5],
+		(float)rot[6], (float)rot[7], (float)rot[8]);
+	
+	// printf("trans vec: \n %.3f %.3f %.3f\n", tv[0], tv[1], tv[2]);	
 
-	printf("trans vec: \n %.3f %.3f %.3f\n", tv[0], tv[1], tv[2]);
-	*/
+	translationVector = glm::vec3(tv[0], tv[1], tv[2]);
 
 	double _pm[12] =
 	{
@@ -370,8 +395,13 @@ void getEulerAngles(
 	};
 
 	cv::Mat tmp, tmp1, tmp2, tmp3, tmp4, tmp5;
-	cv::decomposeProjectionMatrix(cv::Mat(3, 4, CV_64FC1, _pm), tmp, tmp1, tmp2, tmp3, tmp4, tmp5, eav);
 	/*
-	printf("Face Rotation Angle:  %.5f %.5f %.5f\n", eav[0], eav[1], eav[2]);
+	yaw   y
+	pitch x
+	roll  z
 	*/
+	cv::Vec3d eav;
+	cv::decomposeProjectionMatrix(cv::Mat(3, 4, CV_64FC1, _pm), tmp, tmp1, tmp2, tmp3, tmp4, tmp5, eav);	
+	
+	// printf("Face Rotation Angle:  %.5f %.5f %.5f\n", eav[0], eav[1], eav[2]);	
 }
